2343f0eda2bfb946aa2870e23a122b285b9fae83
Admin <Admin@AdminPC>
2017-08-12 17:38:44 +0600

    версия 1

---
+++ Array/src/Test/Array.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+/**
+ *
+ * @author Admin
+ */
+public class Array {
+    public void ShowArray() {
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
